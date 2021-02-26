160e1f999119857a7621189d36240b7203ea9c4f
Sarah Rudy <sarahrudy@gmail.com>
2021-02-26 13:30:23 -0700

    Add model for developer

---
+++ models/model_b.rb
@@ -0,0 +1,9 @@
+# developer model
+name = "Sarah"
+favorite_language = "Ruby"
+age = 31
+height_in_centimeters = 178
+is_team_player = true
+has_positive_attitude = true
+personality_traits = ["analytical", "curious", "empathetic"]
+skills  = ["attention to detail", "time management", "technical experience"]
