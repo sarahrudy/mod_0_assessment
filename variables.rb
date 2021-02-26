157715363da26ba4c51bbb9895cae11da9bb61fc
Sarah Rudy <sarahrudy@gmail.com>
2021-02-26 12:57:57 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Social Network"
+is_entertaining = true
+movie = "Steve Jobs"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["bananas", "chips", "salsa"]
+favorite_foods.shift
+movie.length
