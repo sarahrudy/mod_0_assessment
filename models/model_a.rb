2d954512cac7fa4748dabe833a157b38f71ea21b
Sarah Rudy <sarahrudy@gmail.com>
2021-02-26 13:18:40 -0700

    Add model for dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# dinosaur model
+group = "reptiles"
+first_appearance = "Triassic period"
+first_extinction_in_years_million = 201.3
+height_in_feet = 130
+is_cool = true
+is_extinct = true
+dominance_period = ["Jurassic", "Cretaceous"]
+characteristics = ["lay eggs", "herbivorous", "carnivorous"]
+
+# [reference](https://en.wikipedia.org/wiki/Dinosaur)
